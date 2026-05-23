074c02285a99d687fb83e70afe955b1358d37e65
nonnopomu <codebrew256@gmail.com>
2026-05-24 05:32:30 +0900

    feat: カフェ詳細ページを実装

--- app/controllers/cafes_controller.rb
+++ app/controllers/cafes_controller.rb
@@ -1,7 +1,11 @@
 class CafesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :index ]
+  skip_before_action :authenticate_user!, only: [ :index, :show ]
 
   def index
     @cafes = Cafe.published.includes(:area, :tags, :drink_logs)
+  end
+
+  def show
+    @cafe = Cafe.includes(:area, :tags, :drink_logs).find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :cafes, only: [ :index ]
+  resources :cafes, only: [ :index, :show ]
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
