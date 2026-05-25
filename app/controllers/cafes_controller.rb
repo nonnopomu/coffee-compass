093b6fe17bab907e37b3eacea9dcdec2656b13f7
nonnopomu <codebrew256@gmail.com>
2026-05-26 03:13:55 +0900

    feat: カフェ検索処理を実装

--- app/controllers/cafes_controller.rb
+++ app/controllers/cafes_controller.rb
@@ -3,6 +3,20 @@ class CafesController < ApplicationController
 
   def index
     @cafes = Cafe.published.includes(:area, :tags, :drink_logs)
+
+    if params[:prefectures].present?
+      area_ids = Area.where(prefecture: params[:prefectures]).pluck(:id)
+      @cafes = @cafes.where(area_id: area_ids)
+    end
+
+    if params[:tag_ids].present?
+      @cafes = @cafes.joins(:tags).where(tags: { id: params[:tag_ids] }).distinct
+    end
+
+    if params[:keyword].present?
+      keyword = "%#{params[:keyword]}%"
+      @cafes = @cafes.where("name LIKE ? OR address LIKE ? OR description LIKE ?", keyword, keyword, keyword)
+    end
   end
 
   def show
