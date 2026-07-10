6402054f7fbc73ba871c260ff2589f2f3cc150cb
nonnopomu <codebrew256@gmail.com>
2026-07-10 15:53:37 +0900

    feat: ログイン状態の保持に対応

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,7 @@
 module Users
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+    include Devise::Controllers::Rememberable
+
     def google_oauth2
       auth = request.env["omniauth.auth"]
       return_to = request.env.dig("omniauth.params", "return_to")
@@ -7,6 +9,7 @@ module Users
       @user = User.from_omniauth(auth)
 
       sign_in @user, event: :authentication
+      remember_me(@user)
       set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
       redirect_to stored_location_for(@user) || safe_return_path(root_path, return_to: return_to)
 
