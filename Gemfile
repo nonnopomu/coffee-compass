e158feedd4624d1321eff34c334ae2ce04d70eb8
nonnopomu <codebrew256@gmail.com>
2026-07-31 16:13:28 +0900

    feat: Resendによるメール送信を本番環境に設定

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ gem "aws-sdk-s3", require: false
 gem "omniauth-google-oauth2"
 gem "omniauth-rails_csrf_protection"
 gem "kaminari"
+gem "resend"
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 gem "image_processing", "~> 1.2"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,8 @@ Rails.application.configure do
   # Disable caching for Action Mailer templates even if Action Controller
   # caching is enabled.
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :resend
+  config.action_mailer.default_url_options = { host: "coffeecompass.jp", protocol: "https" }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
