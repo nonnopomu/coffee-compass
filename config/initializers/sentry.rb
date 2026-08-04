27088c1f517094721fe9a29c45bb7ef90a077f8a
nonnopomu <codebrew256@gmail.com>
2026-08-05 04:29:30 +0900

    style: Lint修正

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,6 +2,6 @@ Sentry.init do |config|
   config.dsn = ENV["SENTRY_DSN"]
 
   config.enabled_environments = %w[production]
-  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+  config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
   config.send_default_pii = false
 end
