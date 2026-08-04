79cb58548aaf1043004ab61665d79b30c951fde9
nonnopomu <codebrew256@gmail.com>
2026-08-05 04:24:28 +0900

    feat: Sentryによるエラー監視を導入

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,7 @@
+Sentry.init do |config|
+  config.dsn = ENV["SENTRY_DSN"]
+
+  config.enabled_environments = %w[production]
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+  config.send_default_pii = false
+end
